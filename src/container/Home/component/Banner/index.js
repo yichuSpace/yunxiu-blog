170cc08ea8b18ac416fdf63997384c71e2f517df
hejianzhi <hejianzhi@deepexi.com>
2021-11-03 21:15:00 +0800

    'init'

---
+++ src/container/Home/component/Banner/index.js
@@ -0,0 +1,5 @@
+import styles from "./style.module.scss";
+const Banner = () => {
+  return <div className={styles.bannerBox}>sadf</div>;
+};
+export default Banner;
---
+++ src/container/Home/index.js
@@ -0,0 +1,9 @@
+import Banner from "./component/Banner";
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+    </div>
+  );
+};
+export default Home;
